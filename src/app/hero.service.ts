3df465585bf4021d63dc62c60bd6f61f481b9703
Garcia <edrei.garcia@accenture.com>
2018-01-24 16:57:46 -0800

    hero-search functionality added

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -7,12 +7,16 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { catchError, map, tap } from 'rxjs/operators';
 
 
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
 
 @Injectable()
 export class HeroService {
   // URL to web API, api/<nameOfCollectionReturned>
  // <nameOfCollectionReturned> has to match collection returned in 'in-memory-data.service.ts' file
   private heroesUrl = 'api/heroes';
+
 
   // This is a typical "service-in-service" scenario: you inject the MessageService --
   // into the HeroService which is injected into the HeroesComponent.
@@ -65,6 +69,48 @@ export class HeroService {
       tap(_ => this.log(`Fetched hero id:=${id}`)),
       catchError(this.handleError<Hero>(`getHero id=${id}`))
       );
+  }
+
+  updateHero(hero: Hero) {
+    // The HttpClient.put() method takes 3 params(URL, dataToUpdate, options)
+    // The URL is unchanged. The heroes web API knows which hero to update by looking at the hero's id.
+    return this.http.put(this.heroesUrl, hero, httpOptions).pipe(
+      tap(_ => this.log(`Updated hero id=${hero.id}`)),
+      catchError(this.handleError<any>('updateHero'))
+    );
+  }
+
+  addHero(hero: Hero): Observable<Hero> {
+    return this.http.post<Hero>(this.heroesUrl, hero, httpOptions)
+      .pipe(
+        tap((hero: Hero) => this.log(`Added new hero w/ id=${hero.id}`)),
+        catchError(this.handleError<Hero>('addHero'))
+      );
+  }
+
+  deleteHero(hero: Hero | number): Observable<Hero> {
+    const id = typeof hero === 'number' ? hero : hero.id;
+    const url = `${this.heroesUrl}/${id}`;
+
+    return this.http.delete<Hero>(url, httpOptions).pipe(
+      tap(_ => this.log(`deleted hero id=${id}`)),
+      catchError(this.handleError<Hero>('deleteHero'))
+    );
+  }
+
+  searchHeroes(term: string): Observable<Hero[]> {
+
+    if (!term.trim()) {
+      //if no search term, return empty hero array
+      return of([]);
+    }
+
+    //const url = `api/heroes/?name=${term}`;
+
+    return this.http.get<Hero[]>(`api/heroes/?name=${term}`).pipe(
+      tap(_ => this.log(`found heroes matching "${term}"`)),
+      catchError(this.handleError<Hero[]>('searchHeroes', []))
+    );
   }
 
      // OLD CODE
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -20,5 +20,23 @@ export class HeroesComponent implements OnInit {
     this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
   }
 
+  addHero(name: string): void {
+    name = name.trim();
 
+    if(!name) {return};
+
+    this.heroService.addHero({name} as Hero)
+      .subscribe(hero => {
+        this.heroes.push(hero)
+      });
+
+  }
+
+  deleteHero(hero: Hero): void {
+
+    // this.heroes = this.heroes.filter(h => h !== hero);
+    this.heroService.deleteHero(hero).subscribe(_ => {
+      this.heroes = this.heroes.filter(h => h !== hero)
+    });
+  }
 }
